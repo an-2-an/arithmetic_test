e8c81c8e4c2797d94730db5d244db6aaefd421bc
an-2-an <rjabenkoae@gmail.com>
2022-02-16 13:35:41 +0200

    etap 2

--- arithmetic_test.py
+++ arithmetic_test.py
@@ -1,6 +1,8 @@
 import random
 
 RIGHT, WRONG = 'right!', 'wrong!'
+INCORRECT = 'Incorrect format'
+correct_answers = 0
 
 def get_expression():
     a, b = (random.randint(2, 9) for _ in range(2))
@@ -20,16 +22,22 @@ def get_answer():
 
 
 def main():
-    expression = get_expression()
-    print(expression)
-    try:
-        answer = get_answer()
-        if is_correct(expression=expression, answer=answer):
-            print(RIGHT)
-        else:
-            print('WRONG')
-    except ValueError:
-        pass
+    global correct_answers
+    for i in range(5):
+        expression = get_expression()
+        print(expression)
+        while True:
+            try:
+                answer = get_answer()
+                if is_correct(expression=expression, answer=answer):
+                    print(RIGHT + '\n')
+                    correct_answers += 1
+                else:
+                    print(WRONG + '\n')
+                break
+            except ValueError:
+                print(INCORRECT)
+    print(f'Your mark is {correct_answers}/5.')
 
 
 
